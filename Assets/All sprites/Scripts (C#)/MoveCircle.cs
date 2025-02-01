683de4956c4c306cdaa173d15b10bfa8fe40dba6
Elise Adjeroh <eliseadjeroh@Elises-MacBook-Air.local>
2025-02-01 01:41:11 -0500

    Movement for sprites

---
+++ Assets/All sprites/Scripts (C#)/MoveCircle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCircle : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+         if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        }
+         if(Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+
+     }
+}
